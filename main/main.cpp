bddd6b39a11fdbf2fa2c5bd5fde514aaf774223b
yrs <starovoyt.yaroslav@gmail.com>
2025-07-08 12:51:03 +0300

    Added arduino libraries

--- main/main.cpp
+++ main/main.cpp
@@ -11,7 +11,8 @@
 #include "esp_log.h"
 #include "Arduino.h"
 #include "SparkFun_BNO080_Arduino_Library.h"
-#include "BNOs.h"
+#include "Adafruit_BNO055.h"
+// #include "BNOs.h"
 #include "driver/uart.h"
 
 static const char *TAG = "i2c_scan";
